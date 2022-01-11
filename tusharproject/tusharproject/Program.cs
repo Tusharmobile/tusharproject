433bd2af2e9c6fd50e7d35a9056888e43d667445
tushar <tusharshitre123@gmail.com>
2022-01-11 19:43:38 +0530

    we created a simple project

---
+++ tusharproject/tusharproject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tusharproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How are you");
+            Console.ReadLine();    
+
+        }
+    }
+}
